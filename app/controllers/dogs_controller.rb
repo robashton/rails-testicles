757968b1b5aba726b234415cb30bd2d3b453debb
Rob Ashton <robashton@codeofrob.com>
2013-12-20 10:58:30 +0000

    Can at least check for logged in users before editing a dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,6 @@
 class DogsController < InheritedResources::Base
+  before_filter :authenticate_user!, :except => [ :index, :show ]
+
   def permitted_params
     params.permit(:dog => [:name, :description])
   end
