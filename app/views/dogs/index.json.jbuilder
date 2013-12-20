86cc830ce029d9c60455becd29dbd6f26f47e20b
Rob Ashton <robashton@codeofrob.com>
2013-12-20 10:04:59 +0000

    reset to scratch again

--- app/views/dogs/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@dogs) do |dog|
-  json.extract! dog, :id, :name, :description
-  json.url dog_url(dog, format: :json)
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131220100135) do
+ActiveRecord::Schema.define(version: 20131220100309) do
 
   create_table "dogs", force: true do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
